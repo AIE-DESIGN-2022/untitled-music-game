0307a5c84462ba8862f3b2aba677c63f9ce9d8d9
Alec Seppelt <s201295@students.aie.edu.au>
2022-09-14 15:46:29 +0930

    Finished damage script

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,4 +7,9 @@ public class Health : MonoBehaviour
     public float health = 100;
     public float maxHealth = 100;
     public bool canTakeDmg = true;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
 }
