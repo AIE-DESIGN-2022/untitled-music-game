662259c799eb45ab7d065a9bb6a5cc1e7b83872c
Alec Seppelt <s201295@students.aie.edu.au>
2022-09-14 15:06:54 +0930

    Added TakeDamage script

--- Assets/Scripts/TakeDamage.cs
+++ Assets/Scripts/TakeDamage.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class TakeDamage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float damage;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.gameObject.tag == "Enemy")
+        {
+
+        }
     }
 }
