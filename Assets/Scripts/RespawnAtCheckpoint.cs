d7c6e2d89c60f58cc2b1b47e2e3260add27fd950
Pricetag098 <37175719+Pricetag098@users.noreply.github.com>
2022-09-16 09:27:05 +0930

    Checkpoints

---
+++ Assets/Scripts/RespawnAtCheckpoint.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnAtCheckpoint : MonoBehaviour
+{
+    public Checkpoint lastCheckpoint;
+    public void OnDeath()
+    {
+        transform.position = lastCheckpoint.transform.position;
+        Health health = GetComponent<Health>();
+        health.health = health.maxHealth;
+    }
+}
