e13d2fe8b0a6cb5c94c818ec6c3fe79bda3a8230
Pricetag098 <37175719+Pricetag098@users.noreply.github.com>
2022-09-16 10:44:01 +0930

    Some Stuff

---
+++ Assets/Scripts/LevelLoadButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+public class LevelLoadButton : MonoBehaviour
+{
+    int level;
+    Button button;
+    TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        level = transform.GetSiblingIndex()+1;
+        text = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        text.text = level.ToString();
+        button = GetComponent<Button>();
+        //check if level is available
+        if(SaveManager.instance.HighestLevel > level)
+        {
+            button.interactable = false;
+        }
+    }
+
+    public void Load()
+    {
+        SceneManager.LoadScene(level);
+    }
+}
